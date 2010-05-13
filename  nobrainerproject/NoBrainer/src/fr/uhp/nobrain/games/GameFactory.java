8b514a24ef5e8a14042b267c3f3a684d10cd26a7
christophe.labedan <christophe.labedan@eba294d8-33d9-75fe-87ac-0012458e6421>
2010-05-13 13:34:59 +0000

    Suppression de classes inutiles

---  nobrainerproject/NoBrainer/src/fr/uhp/nobrain/games/GameFactory.java
+++
@@ -1,5 +0,0 @@
-package fr.uhp.nobrain.games;
-
-public abstract class GameFactory {
-
-}
---  nobrainerproject/NoBrainer/src/fr/uhp/nobrain/games/GraphicContext.java
+++
@@ -1,9 +0,0 @@
-package fr.uhp.nobrain.games;
-
-import javax.swing.JPanel;
-
-public class GraphicContext extends JPanel{
-
-	private static final long serialVersionUID = 1L;
-
-}
